daf186d60815034f4c4fbf94446317d15a2deac0
Chai Phonbopit <phonbopit@gmail.com>
2018-11-03 16:03:33 +0700

    Finish example

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'API_TOKEN',
+  authDomain: 'hello-firebase-auth-3e611.firebaseapp.com',
+  databaseURL: 'https://hello-firebase-auth-3e611.firebaseio.com',
+  projectId: 'hello-firebase-auth-3e611',
+  storageBucket: 'hello-firebase-auth-3e611.appspot.com',
+  messagingSenderId: 'MESSAGE_ID'
+}
+
+export default config
---
+++ src/firebase/index.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import config from './config'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export default firebase.auth()
